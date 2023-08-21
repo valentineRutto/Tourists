738f28fbd2ab7d4dd25e959d6026adf972af2f97
valentinerutto <rutto28@gmail.com>
2023-08-21 11:23:47 +0300

    -Setup Api service and appmodules

--- app/src/main/java/com/valentinerutto/tourists/data/local/AppDatabase.kt
+++ app/src/main/java/com/valentinerutto/tourists/data/local/AppDatabase.kt
@@ -2,11 +2,9 @@ package com.valentinerutto.tourists.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.migration.AutoMigrationSpec
 import com.valentinerutto.tourists.data.local.dao.TouristDao
 
 @Database(exportSchema = true, version = 1, entities = [TouristEntity::class])
 abstract class AppDatabase: RoomDatabase() {
     abstract fun touristDao(): TouristDao
-    class MyAutomigration: AutoMigrationSpec{}
 }
