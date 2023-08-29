1d86ff5ab4a88364a40b2f9418b850a6b567dd56
valentinerutto <rutto28@gmail.com>
2023-08-29 10:17:39 +0300

    -ui for detail screen

--- app/src/main/java/com/valentinerutto/tourists/ui/theme/views/TouristDetailScreen.kt
+++ app/src/main/java/com/valentinerutto/tourists/ui/theme/views/TouristDetailScreen.kt
@@ -1,25 +1,40 @@
 package com.valentinerutto.tourists.ui.theme.views
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.valentinerutto.tourists.ui.TouristsViewmodel
 
 @Composable
-fun TouristDetailScreen(touristsViewmodel: TouristsViewmodel = viewModel(),
-                        touristItemPosition:Int) {
+fun TouristDetailScreen(
+    modifier: Modifier = Modifier, touristsViewmodel: TouristsViewmodel = viewModel(),
+    touristItemPosition: Int
+) {
 
     val tourists = touristsViewmodel.tourists.collectAsState().value.get(touristItemPosition)
-  Column(){
-        Text(
-            text = tourists.name,
-            fontWeight = FontWeight.SemiBold,
-            fontSize = 20.sp
-        )
+//
+    Box(modifier = modifier
+        .fillMaxSize()
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+//
+        Column(modifier = Modifier.padding(32.dp)) {
+            Text(
+                text = tourists.name,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Text(text = tourists.email)
+
+        }
     }
 }
